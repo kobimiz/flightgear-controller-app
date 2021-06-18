edc593073d653d73f3019b04eaa55b4618a2398f
tom <zliltom@gmail.com>
2021-06-19 02:19:06 +0300

    interfacing flightgear

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.app
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import java.io.*
 import java.net.Socket
@@ -14,66 +14,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        val thread = Thread {
-            try {
-                println("trying to connect")
-                val fg = Socket("10.0.0.10", 6400)
-                println("connected")
-                // FILE IS NOT FOUND
-                val input = BufferedReader(FileReader("reg_flight.csv"))
-                val out = PrintWriter(fg.getOutputStream())
-                var line: String
-                while(true) {
-                    out.flush()
-                    line = input.readLine()
-                    if (line == null)
-                        break
-                }
-                out.close()
-                input.close()
-                fg.close()
+        val connect = findViewById<Button>(R.id.connect)
+        val change = findViewById<Button>(R.id.change)
+        val disconnect = findViewById<Button>(R.id.disconnect)
+        val valuetext = findViewById<EditText>(R.id.valuetext)
+        val valuenametext = findViewById<EditText>(R.id.valuenametext)
 
-//                Log.d("TAG", "trying to connect to 10.0.0.10")
-//                val fg = Socket("10.0.0.10", 6400)
-//                fg.outputStream.write("get /controls/flight/aileron[0]".toByteArray())
-////                val out = PrintWriter(fg.getOutputStream(), true)
-////                out.println("get /controls/flight/aileron[0]")
-//
-//                val reader = BufferedReader(
-//                    InputStreamReader(fg.getInputStream())
-//                )
-//                val str = reader.readLine()
-//                println("Input Stream: $str")
-//                println("after read")
+        var fg : Socket? = null
+        var out : PrintWriter? =null
+        var input : BufferedReader? = null
 
-//                println("Input Stream: $fg.getInputStream().read()")
-//
-//                var readChar: Int
-//                while (true) {
-//                    readChar = fg.getInputStream().read()
-
-//                    if (readChar <= -1)
-//                        break
-//                }
-
-//                try {
-//                    val inputStream = DataInputStream(
-//                        BufferedInputStream(fg.getInputStream()))
-//
-//                    println("Input Stream: ${inputStream.readInt()}")
-//                    println("after read")
-//                } catch (e : java.lang.Exception) {
-//                    e.printStackTrace()
-//                }
-                fg.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+        fun start() {
+            println("trying to connect")
+            fg = Socket("10.0.2.2", 6400)
+            println("connected")
+            out = PrintWriter(fg!!.getOutputStream(), true)
+            input = BufferedReader(InputStreamReader(fg!!.getInputStream()))
         }
 
-        button.setOnClickListener{
+        fun stop() {
+            input!!.close()
+            out!!.close()
+            fg!!.close()
+            println("closed connection")
+        }
+
+        fun changeValue(type:String,value:String) {
+            println("printing:"+"set /controls/flight/"+type+" "+value+"\r\n")
+            out!!.print("set /controls/flight/"+type+" "+value+"\r\n")
+            out!!.flush()
+            val resp: String = input!!.readLine()
+            println("response: "+resp)
+
+        }
+
+        connect.setOnClickListener{
+            val thread = Thread {
+                try {
+                    start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            thread.start()
+
+        }
+        change.setOnClickListener{
+            val thread = Thread {
+                try {
+                    val valuename = valuenametext.text.toString()
+                    val value = valuetext.text.toString()
+                    changeValue(valuenametext.text.toString(),valuetext.text.toString())
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
             thread.start()
         }
+        disconnect.setOnClickListener{
+            val thread = Thread {
+                try {
+                    stop()
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            thread.start()
+        }
+
     }
 }
